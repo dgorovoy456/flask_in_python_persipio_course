1249d3ecd966687273d5a90b7324cb0c82483041
denys.horovyi <denys.horovyi@globallogic.com>
2022-03-17 11:33:08 +0100

    For basic_flask_app_p4.py:
    - feedback.html added
    - mapping to feedback form via feedback_button in main page added

--- flask/basic_flask_app_p4.py
+++ flask/basic_flask_app_p4.py
@@ -8,5 +8,10 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/feedback')
+def feedback():
+    return render_template('feedback.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
